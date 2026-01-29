cf716e0570dbf36135a187986ae1d0ab00fb25c7
Ghena-Dayeh <ghinadd7@gmail.com>
2026-01-30 01:12:31 +0300

    edit and animations

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -17,8 +17,10 @@ const Products = () => {
           <ProductOptions />
           <ShippingInfo />
           <ProductFeatures />
-          <FaqSection/>
         </div>
+      </section>
+      <section>
+        <FaqSection/>
       </section>
     </>
   );
